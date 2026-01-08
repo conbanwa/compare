21698af0074ff8ca79ec33f3c31dcfd09b1434da
conbanwa <35380552+conbanwa@users.noreply.github.com>
2026-01-08 11:12:14 +0800

    Legacy

--- bench_test.go
+++ bench_test.go
@@ -12,19 +12,25 @@ func BenchmarkAreEqual(b *testing.B) {
 
 func BenchmarkAreUnique(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Unique(b, i, i, i)
+		Unique(b, i, i+1, i+2)
 	}
 }
 
 func BenchmarkGreater(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Greater(-1)(i)
+		Greater(i)(i+1)
+	}
+}
+
+func BenchmarkGreaterLegacy(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		if i+1>i{}
 	}
 }
 
 func BenchmarkGreaterOrEqual(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		GreaterOrEqual(0)(i)
+		GreaterOrEqual(i)(i)
 	}
 }
 
@@ -38,6 +44,6 @@ func BenchmarkGreaterThan(b *testing.B) {
 func BenchmarkGreaterThanEqual(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		j := float64(i)
-		GreaterOrEqual(-0.0)(j)
+		GreaterOrEqual(i)(j)
 	}
 }
